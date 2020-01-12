66132faa80223faa7467d5cee3f4788bf40706d4
valeravarchak <valeraavarchak@gmail.com>
2020-01-12 15:50:18 +0200

    Added Highlight

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './resources/styles.css';
 import Header from './components/header_footer/header.js';
 import Featured from "./components/featured";
 import VenueNfo from "./components/venueNfo";
+import Highlight from "./components/Highlight";
 
 
 function App() {
   return (
-    <div className="App" style={{ height:"1500px",background:'cornflowerblue'}}>
+    <div className="App">
       <Header/>
       <Featured/>
       <VenueNfo/>
+      <Highlight/>
     </div>
   );
 }
---
+++ src/components/Highlight/Description.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import  Fade from 'react-reveal/Fade';
+
+const Description = () => {
+    return (
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlight</h2>
+                <div className="highlight_description ">
+                    Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Ad aliquid blanditiis de
+                    leniti dignissimos, dolor dolores ducimus eligendi
+                    fuga illo iure iusto labore libero possimus qui quidem rem repellat
+                    tempora unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis deserunt enim et fugiat hic laboriosam
+                    numquam, officia omnis porro quaerat quibusdam quis rerum saepe soluta totam, vel velit voluptates voluptatum.
+                </div>
+            </div>
+        </Fade>
+    );
+};
+
+export default Description;
